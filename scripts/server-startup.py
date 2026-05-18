206a89376725a582fac19fe8ac07ada44467f762
Brandon Miller <brandonmiller.tm@gmail.com>
2026-05-18 17:17:53 -0400

    WIP: Python controller, some notes.

---
+++ scripts/server-startup.py
@@ -0,0 +1,17 @@
+# Client to server Wake-On-LAN Packet
+import subprocess
+import time
+
+# This .py file is intended to connect the Pi to the Server/PC.
+# The Pi will essentially function as a primtive website which interfaces a server. 
+# UNFINISHED; WORK ON WAKE ON LAN!!
+
+END_MAC = "AA:BB:CC:DD:EE:FF"
+END_IP = "192.168.1.1"
+
+def powerEndDevice():
+    # 'wakeonlan' required for Raspberry Pi.
+    subprocess.run("wakeonlan",shell=True)
+
+def startMinecraftServer():
+    subprocess.run('ssh user@pc "systemctl start minecraft"', shell= True)
