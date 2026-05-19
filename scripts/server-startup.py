3d763d5ee767b33a232e9c922054186e814236e9
Brandon Miller <brandonmiller.tm@gmail.com>
2026-05-19 03:49:26 -0400

    Added website stuff.

--- scripts/server-startup.py
+++ scripts/server-startup.py
@@ -4,7 +4,7 @@ import time
 
 # This .py file is intended to connect the Pi to the Server/PC.
 # The Pi will essentially function as a primtive website which interfaces a server. 
-# UNFINISHED; WORK ON WAKE ON LAN!!
+# UNFINISHED; WORK ON WAKE ON LAN!! ok
 
 END_MAC = "AA:BB:CC:DD:EE:FF"
 END_IP = "192.168.1.1"
@@ -14,4 +14,4 @@ def powerEndDevice():
     subprocess.run("wakeonlan",shell=True)
 
 def startMinecraftServer():
-    subprocess.run('ssh user@pc "systemctl start minecraft"', shell= True)
+    subprocess.run('ssh bigsp "systemctl start minecraft"', shell= True)
